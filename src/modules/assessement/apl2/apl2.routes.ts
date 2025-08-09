3b0c7275863b63cff785c7a26c143a13dd1aad77
Jeki <ahmadzaqi490@gmail.com>
2025-08-09 20:57:51 +0700

    [UPDATE] Add new endpoints

--- src/modules/assessement/apl2/apl2.routes.ts
+++ src/modules/assessement/apl2/apl2.routes.ts
@@ -5,7 +5,9 @@ const router = Router();
 const controller = new APL2Controller();
 
 router.post('/create-assessment', controller.createAssessment.bind(controller));
-router.get('/assessments', controller.getAssessments.bind(controller));
-router.get('/assessments/:id', controller.getAssessmentById.bind(controller));
+router.get('', controller.getAssessments.bind(controller));
+router.get('/:id', controller.getAssessmentById.bind(controller));
+router.get('/unit-competencies/:assessmentId', controller.getUnitCompetenciesByAssessmentId.bind(controller));
+router.get('/unit-competencies/elements/:unitCompetencyId', controller.getElementsByUnitCompetencyId.bind(controller));
 
 export default router;
