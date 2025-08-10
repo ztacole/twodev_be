fa8e582fc891531d1c31d350747c45447cbe52e0
Jeki <ahmadzaqi490@gmail.com>
2025-08-10 18:35:50 +0700

    [FIX] Fix typo in route

--- src/modules/assessement/apl2/apl2.routes.ts
+++ src/modules/assessement/apl2/apl2.routes.ts
@@ -5,7 +5,7 @@ const router = Router();
 const controller = new APL2Controller();
 
 router.post('/create-assessment', controller.createAssessment.bind(controller));
-router.get('', controller.getAssessments.bind(controller));
+router.get('/', controller.getAssessments.bind(controller));
 router.get('/:id', controller.getAssessmentById.bind(controller));
 router.get('/unit-competencies/:assessmentId', controller.getUnitCompetenciesByAssessmentId.bind(controller));
 router.get('/unit-competencies/elements/:unitCompetencyId', controller.getElementsByUnitCompetencyId.bind(controller));
