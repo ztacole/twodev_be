41b4459f275e2c6e540e6851223f1a363c354ab5
Jeki <ahmadzaqi490@gmail.com>
2025-08-24 20:12:02 +0700

    [NEW & UPDATE] Add new endpoint for IA-01 and change DB setup

--- src/modules/assessement/ia-01/ia-01.controller.ts
+++ src/modules/assessement/ia-01/ia-01.controller.ts
@@ -1,16 +1,51 @@
 import { IA01Service } from "./ia-01.service";
 import { asyncHandler } from "../../../common/async.handler";
+import { AssessorApproveRequest, SendResultRequest } from "./ia-01.type";
 
 export class IA01Controller {
     static getIA01Groups = asyncHandler(async (req, res) => {
-        const assessmentId = Number(req.params.assessmentId);
-        const iaGroups = await IA01Service.getIA01Groups(assessmentId);
+        const resultId = Number(req.params.resultId);
+        if (!resultId) {
+            throw new Error('Result ID is required');
+        }
+
+        const iaGroups = await IA01Service.getIA01Groups(resultId);
         res.status(200).json({ success: true, message: 'Group IA berhasil diambil', data: iaGroups });
     });
 
     static getElementsByUnitId = asyncHandler(async (req, res) => {
+        const resultId = Number(req.params.resultId);
         const unitId = Number(req.params.unitId);
-        const elements = await IA01Service.getElementsByUnitId(unitId);
+        if (!resultId || !unitId) {
+            throw new Error('Result ID and Unit ID are required');
+        }
+
+        const elements = await IA01Service.getElementsByUnitId(resultId, unitId);
         res.status(200).json({ success: true, message: 'Elemen berhasil diambil', data: elements });
+    });
+
+    static sendResult = asyncHandler(async (req, res) => {
+        const data: SendResultRequest = req.body;
+        const result = await IA01Service.sendResult(data);
+        res.status(200).json({ success: true, message: 'Hasil berhasil dikirimkan', data: result });
+    });
+
+    static approvedByAssessor = asyncHandler(async (req, res) => {
+        const resultId = Number(req.params.resultId);
+        if (!resultId) {
+            throw new Error('Result ID is required');
+        }
+        const data: AssessorApproveRequest = req.body;
+        const result = await IA01Service.approvedByAssessor(resultId, data);
+        res.status(200).json({ success: true, message: 'Hasil berhasil dikirimkan', data: result });
+    });
+
+    static approvedByAssessee = asyncHandler(async (req, res) => {
+        const resultId = Number(req.params.resultId);
+        if (!resultId) {
+            throw new Error('Result ID is required');
+        }
+        const result = await IA01Service.approvedByAssessee(resultId);
+        res.status(200).json({ success: true, message: 'Hasil berhasil dikirimkan', data: result });
     });
 }
