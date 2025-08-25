b6b0fdde117c451928651ccebcedc4fd51277b67
Rifq11 <rifqikamaluddin11@gmail.com>
2025-08-25 20:30:47 +0700

    [UPDATE] route

--- src/modules/assessement/assessment.routes.ts
+++ src/modules/assessement/assessment.routes.ts
@@ -83,7 +83,7 @@ router.get('/ak', AKController.getAllAK);
 router.post('/ak-03', AK03Controller.createAK03);
 router.get('/ak-03/:result_id', AK03Controller.getAK03ByResultId);
 router.post('/ak-04', AK04Controller.createAK04);
-router.get('/ak-04/result/:resultId', AK04Controller.getAK04ByResultId);
+router.get('/ak-04/:resultId', AK04Controller.getAK04ByResultId);
 router.post('/ak-05', AK05Controller.createAK05);
 router.get('/ak-05/:result_id', AK05Controller.getAK05ByResultId);
 
