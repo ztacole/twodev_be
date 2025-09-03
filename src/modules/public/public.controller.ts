2527e830ff10f7384f79871e1c6033ba24d1ae22
Jeki <ahmadzaqi490@gmail.com>
2025-09-03 20:43:40 +0700

    [UPDATE] Update public route

--- src/modules/public/public.controller.ts
+++ src/modules/public/public.controller.ts
@@ -4,13 +4,35 @@ import { Request, Response } from "express";
 export class PublicController {
     public static async getAssesseeById(req: Request, res: Response) {
         const id = Number(req.params.id);
+        if (!id) {
+            return res.status(400).json({
+                success: false,
+                message: 'ID asesi diperlukan'
+            });
+        }
+
         const assessee = await PublicService.getAssesseeById(id);
-        res.status(200).json(assessee);
+        res.status(200).json({
+            success: true,
+            message: 'Data asesi berhasil diambil',
+            data: assessee
+        });
     }
 
     public static async getAssessorById(req: Request, res: Response) {
         const id = Number(req.params.id);
+        if (!id) {
+            return res.status(400).json({
+                success: false,
+                message: 'ID asesor diperlukan'
+            });
+        }
+
         const assessor = await PublicService.getAssessorById(id);
-        res.status(200).json(assessor);
+        res.status(200).json({
+            success: true,
+            message: 'Data asesor berhasil diambil',
+            data: assessor
+        });
     }
 }
