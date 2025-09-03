814857032450700da8ec306d6c631bbb03523d01
Jean <jeanarby030108@gmail.com>
2025-09-03 22:12:06 +0700

    fix: response in ak-05

--- src/modules/assessement/ak-05/ak-05.service.ts
+++ src/modules/assessement/ak-05/ak-05.service.ts
@@ -41,7 +41,33 @@ export class AK05Service {
   static async getAK05ByResultId(result_id: number): Promise<AK05Response | null> {
     const ak05 = await prisma.result_ak05.findUnique({
       where: { result_id },
-    });
+      include: {
+        result: {
+          include: {
+            assessment: {
+              include: {
+                occupation: {
+                  include: {
+                    scheme: true
+                  }
+                }
+              }
+            },
+            assessee: {
+              include: {
+                user: true
+              }
+            },
+            assessor: {
+              include: {
+                user: true
+              }
+            },
+            result_ak05: true
+          }
+        }
+      }
+    })
 
     return ak05 ? formatAK05Response(ak05) : null;
   }
@@ -60,12 +86,24 @@ export class AK05Service {
 function formatAK05Response(ak05: any): AK05Response {
   return {
     id: ak05.id,
-    result_id: ak05.result_id,
+    result: {
+    id: ak05.result.id,
+    assessment: ak05.result.assessment,
+    assessee: {
+      id: ak05.result.assessee.id,
+      name: ak05.result.assessee.user.full_name,
+      email: ak05.result.assessee.user.email,
+    },
+    assessor: {
+      id: ak05.result.assessor.id,
+      name: ak05.result.assessor.user.full_name,
+      email: ak05.result.assessor.user.email,
+      no_reg_met: ak05.result.assessor.no_reg_met,
+    },
+    tuk: ak05.result.tuk,
+    created_at: ak05.result.created_at,
+    result_ak05: ak05.result.result_ak05,
+    },
     is_competent: ak05.is_competent,
-    description: ak05.description,
-    negative_positive_aspects: ak05.negative_positive_aspects,
-    rejection_notes: ak05.rejection_notes,
-    improvement_suggestions: ak05.improvement_suggestions,
-    approved_assessor: ak05.approved_assessor,
   };
 }
