3534fbc3972544cb14a7ad292f32bc18ab3e990b
Jean <jeanarby030108@gmail.com>
2025-09-16 14:04:47 +0700

    fix: folder

--- src/modules/assessement/apl-01/upload-config.ts
+++ src/modules/assessement/apl-01/upload-config.ts
@@ -7,7 +7,7 @@ const storage = multer.diskStorage({
     const assesseeId = req.params?.assessee_id || req.body?.assessee_id || 'unknown';
     const assessorId = req.params?.assessor_id || req.body?.assessor_id || 'unknown';
     const assessmentId = req.params?.assessment_id || req.body?.assessment_id || 'unknown';
-    const uploadPath = path.join(__dirname, '../../../../public/uploads/apl-01', `${assesseeId}_${assessorId}_${assessmentId}`);
+    const uploadPath = path.join(process.cwd(), 'public/uploads/apl-01', `${assesseeId}_${assessorId}_${assessmentId}`);
     
     if (!fs.existsSync(uploadPath)) {
       fs.mkdirSync(uploadPath, { recursive: true });
