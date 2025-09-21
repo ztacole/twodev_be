e049a57bc6958a05bc2803d71ac55cd8895065f2
Jeki <ahmadzaqi490@gmail.com>
2025-09-21 12:56:22 +0700

    pref: fiz response assessor lagi

--- src/modules/assessor/assessor.type.ts
+++ src/modules/assessor/assessor.type.ts
@@ -3,6 +3,7 @@ export interface AssessorResponse {
     id: number;
     user_id: number;
     name: string;
+    email: string;
     birth_location: string;
     birth_date: Date;
     no_reg_met: string;
